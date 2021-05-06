e87617d5f3dae8866704d2fd0194835695ac00b8
H4ckint0sh <alijan.adeli@icloud.com>
2021-05-06 17:04:26 +0200

    made the app a PWA

--- components/Meta.js
+++ components/Meta.js
@@ -8,6 +8,7 @@ const Meta = ({ title, keywords, description }) => (
     <meta name="description" content={description} />
     <meta charSet="utf-8" />
     <link rel="icon" href="/favicon.ico" />
+    <link rel="manifest" href="/manifest.json" />
     <title>{title}</title>
   </Head>
 );
---
+++ pages/fallback.js
@@ -0,0 +1,10 @@
+const Fallback = () => (
+  <>
+    <div className="container">
+      <h1>This is offline fallback page</h1>
+      <h2>When offline, any route will fallback to this page</h2>
+    </div>
+  </>
+);
+
+export default Fallback;
